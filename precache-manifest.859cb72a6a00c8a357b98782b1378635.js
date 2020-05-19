898f814d6550a16198047c9375678752b0e9e775
Aman Kapoor <aman.kapoor@collegedunia.com>
2020-05-19 15:21:59 +0530

    Updates

---
+++ precache-manifest.859cb72a6a00c8a357b98782b1378635.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7d263c896822ab89f6df41d719ee65",
+    "url": "/search-app/index.html"
+  },
+  {
+    "revision": "8ac8b6c8ab8b52ec3b4a",
+    "url": "/search-app/static/css/main.35361406.chunk.css"
+  },
+  {
+    "revision": "208a3fad8ce590503996",
+    "url": "/search-app/static/js/2.be41aa55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/search-app/static/js/2.be41aa55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ac8b6c8ab8b52ec3b4a",
+    "url": "/search-app/static/js/main.096b22a3.chunk.js"
+  },
+  {
+    "revision": "83b8d9ffc31fcb32522c",
+    "url": "/search-app/static/js/runtime-main.36ed9dd1.js"
+  },
+  {
+    "revision": "de50e2e9ca0fc52b57dbe9600b542e19",
+    "url": "/search-app/static/media/loader.de50e2e9.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/search-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
